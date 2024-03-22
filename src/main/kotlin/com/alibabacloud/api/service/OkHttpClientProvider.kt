48f92592ead00c25f2733ca4a0c0a70635415ff5
zpp <865287328@qq.com>
2024-03-22 16:06:56 +0800

    fix: support accept non-trusted certificates and avoid MIMT

--- src/main/kotlin/com/alibabacloud/api/service/OkHttpClientProvider.kt
+++ src/main/kotlin/com/alibabacloud/api/service/OkHttpClientProvider.kt
@@ -1,10 +1,14 @@
 package com.alibabacloud.api.service
 
+import com.intellij.util.net.ssl.CertificateManager
 import okhttp3.Dispatcher
 import okhttp3.OkHttpClient
 import okhttp3.Protocol
+import java.security.SecureRandom
 import java.util.*
 import java.util.concurrent.TimeUnit
+import javax.net.ssl.SSLContext
+import javax.net.ssl.X509TrustManager
 
 object OkHttpClientProvider {
     private val dispatcher = Dispatcher().apply {
@@ -13,8 +17,13 @@ object OkHttpClientProvider {
     }
 
     val instance: OkHttpClient by lazy {
+        val trustManager = CertificateManager.getInstance().trustManager as X509TrustManager
+        val sslContext = SSLContext.getInstance("TLS")
+        sslContext.init(null, arrayOf(trustManager), SecureRandom())
+
         OkHttpClient.Builder()
             .dispatcher(dispatcher)
+            .sslSocketFactory(sslContext.socketFactory, trustManager)
             .protocols(Collections.singletonList(Protocol.HTTP_1_1))
             .connectTimeout(10, TimeUnit.SECONDS)
             .readTimeout(15, TimeUnit.SECONDS)
