2bb2a8808a258cf3dfa09f764e3887c145a98011
zpp <865287328@qq.com>
2024-07-04 20:41:22 +0800

    fix: provide clearer indication of the current user profile

--- src/main/kotlin/com/alibabacloud/icons/ToolkitIcons.kt
+++ src/main/kotlin/com/alibabacloud/icons/ToolkitIcons.kt
@@ -5,6 +5,7 @@ import com.intellij.openapi.util.IconLoader
 object ToolkitIcons {
     val LOGO_ICON = IconLoader.getIcon("/icons/logo.svg", ToolkitIcons::class.java)
     val API_ICON = IconLoader.getIcon("/icons/api.svg", ToolkitIcons::class.java)
+    val API_DEPRECATED_ICON = IconLoader.getIcon("/icons/deprecated.svg", ToolkitIcons::class.java)
     val API_SELECTED_ICON = IconLoader.getIcon("/icons/api-selected.svg", ToolkitIcons::class.java)
     val TOOLWINDOW_ICON = IconLoader.getIcon("/icons/toolwindow.svg", ToolkitIcons::class.java)
     val PRODUCT_ICON = IconLoader.getIcon("/icons/product.svg", ToolkitIcons::class.java)
