3a2f09c46f31890724ab416166a48d232f243fdb
zpp <865287328@qq.com>
2024-09-02 21:48:48 +0800

    feat: support AK inspections in code 222-241

--- src/main/kotlin/com/alibabacloud/settings/ToolkitSettingsConfigurable.kt
+++ src/main/kotlin/com/alibabacloud/settings/ToolkitSettingsConfigurable.kt
@@ -11,11 +11,13 @@ import javax.swing.JComponent
 class ToolkitSettingsConfigurable : SearchableConfigurable {
     var enableToolkitAutoUpdate: JBCheckBox = JBCheckBox()
     var enableCompletion: JBCheckBox = JBCheckBox()
+    private var enableAKInspection: JBCheckBox = JBCheckBox()
 
     private fun saveBaseSettings() {
         val settingsState = ToolkitSettingsState.getInstance()
         settingsState.isAutoUpdateEnabled = enableToolkitAutoUpdate.isSelected
-        settingsState.isCompletionEnabled= enableCompletion.isSelected
+        settingsState.isCompletionEnabled = enableCompletion.isSelected
+        settingsState.isAKInspectionEnabled = enableAKInspection.isSelected
     }
 
 
@@ -36,6 +38,18 @@ class ToolkitSettingsConfigurable : SearchableConfigurable {
                 text("自动插入 SDK 示例代码（或通过快捷键 ctrl + cmd + p 切换）")
             }
         }
+        group("明文AK/SK检查设置") {
+            row {
+                cell(enableAKInspection).applyToComponent {
+                    this.isSelected = ToolkitSettingsState.getInstance().isAKInspectionEnabled
+                }
+                text(
+                    "开启明文AK/SK检查（强烈建议保持开启）<br/><br/>" +
+                            "<font color='gray' size='-1'>阿里云主账号拥有资源的全部权限，AK一旦泄露，会给系统带来巨大风险，不建议通过明文使用。</font><br/>" +
+                            "<font color='gray' size='-1'>我们只通过正则匹配您代码中的明文AK/SK，不会进行任何保存/上传。</font><br/>"
+                )
+            }
+        }
         group("用户反馈") {
             row {
                 text("<a>反馈链接</a>") {
@@ -47,7 +61,8 @@ class ToolkitSettingsConfigurable : SearchableConfigurable {
 
     override fun isModified(): Boolean {
         return enableToolkitAutoUpdate.isSelected != ToolkitSettingsState.getInstance().isAutoUpdateEnabled ||
-                enableCompletion.isSelected != ToolkitSettingsState.getInstance().isCompletionEnabled
+                enableCompletion.isSelected != ToolkitSettingsState.getInstance().isCompletionEnabled ||
+                enableAKInspection.isSelected != ToolkitSettingsState.getInstance().isAKInspectionEnabled
     }
 
     override fun apply() {
@@ -58,6 +73,7 @@ class ToolkitSettingsConfigurable : SearchableConfigurable {
         val settingsState = ToolkitSettingsState.getInstance()
         enableToolkitAutoUpdate.isSelected = settingsState.isAutoUpdateEnabled
         enableCompletion.isSelected = settingsState.isCompletionEnabled
+        enableAKInspection.isSelected = settingsState.isAKInspectionEnabled
     }
 
     override fun getDisplayName(): String {
