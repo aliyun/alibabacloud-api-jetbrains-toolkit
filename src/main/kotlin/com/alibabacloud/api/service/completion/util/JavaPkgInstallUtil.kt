675db11684b1b4b374d15f2eae6e3b52b66233c4
zpp <865287328@qq.com>
2024-09-13 11:30:31 +0800

    feat: support upgrade maven package

--- src/main/kotlin/com/alibabacloud/api/service/completion/util/JavaPkgInstallUtil.kt
+++ src/main/kotlin/com/alibabacloud/api/service/completion/util/JavaPkgInstallUtil.kt
@@ -51,6 +51,41 @@ class JavaPkgInstallUtil {
                 })
         }
 
+        fun updateMavenDeps(
+            project: Project,
+            mavenCommand: String,
+        ) {
+            val basePath = project.basePath
+            val projectBaseDir = basePath?.let { path ->
+                LocalFileSystem.getInstance().findFileByPath(path)
+            }
+            val pomVirtualFile = projectBaseDir?.findChild("pom.xml")
+
+            updateDependencyInPom(
+                project,
+                pomVirtualFile,
+                mavenCommand,
+                onSuccess = {
+                    NormalNotification.showMessage(
+                        project,
+                        NotificationGroups.DEPS_NOTIFICATION_GROUP,
+                        I18nUtils.getMsg("auto.install.package.update.begin"),
+                        "",
+                        NotificationType.INFORMATION
+                    )
+                },
+                onFailure = {
+                    NormalNotification.showMessage(
+                        project,
+                        NotificationGroups.DEPS_NOTIFICATION_GROUP,
+                        I18nUtils.getMsg("auto.install.package.fail"),
+                        I18nUtils.getMsg("network.check"),
+                        NotificationType.ERROR
+                    )
+                })
+        }
+
+
         private fun insertDependencyInPom(
             project: Project,
             pomVirtualFile: VirtualFile?,
@@ -101,6 +136,52 @@ class JavaPkgInstallUtil {
             }
         }
 
+        private fun updateDependencyInPom(
+            project: Project,
+            pomVirtualFile: VirtualFile?,
+            mavenCommand: String,
+            onSuccess: () -> Unit,
+            onFailure: () -> Unit
+        ) {
+            WriteCommandAction.runWriteCommandAction(project) {
+                val documentManager = FileDocumentManager.getInstance()
+                val document = documentManager.getDocument(pomVirtualFile!!) ?: return@runWriteCommandAction
+                val pomBackupContent = document.text
+                ApplicationManager.getApplication().runReadAction {
+                    try {
+                        val commandInfo = parseMavenCommand(mavenCommand)
+                        val newVersion = commandInfo[0]
+                        val dependencyPattern = commandInfo[1]
+
+                        val updatedContent =
+                            pomBackupContent.replace(dependencyPattern.toRegex(setOf(RegexOption.DOT_MATCHES_ALL))) { matchResult ->
+                                val matchedDependency = matchResult.value
+                                matchedDependency.replace(
+                                    Regex("<version>[^<]+</version>"),
+                                    "<version>$newVersion</version>"
+                                )
+                            }
+                        ApplicationManager.getApplication().invokeLater {
+                            WriteCommandAction.runWriteCommandAction(project) {
+                                document.setText(updatedContent)
+                                documentManager.saveDocument(document)
+                            }
+                        }
+                        refreshMavenProject(project)
+                        onSuccess()
+                    } catch (e: IOException) {
+                        ApplicationManager.getApplication().invokeLater {
+                            WriteCommandAction.runWriteCommandAction(project) {
+                                document.setText(pomBackupContent)
+                                documentManager.saveDocument(document)
+                            }
+                            onFailure()
+                        }
+                    }
+                }
+            }
+        }
+
         private fun refreshMavenProject(project: Project) {
             val mavenReimportAction = ActionManager.getInstance().getAction("Maven.Reimport")
             if (mavenReimportAction != null) {
@@ -118,6 +199,7 @@ class JavaPkgInstallUtil {
         ): MutableList<Boolean> {
             var isDependencyExists = false
             var isPomExists = false
+            var needUpdate = false
             val basePath = project.basePath
             val projectBaseDir = basePath?.let { path ->
                 LocalFileSystem.getInstance().findFileByPath(path)
@@ -131,29 +213,45 @@ class JavaPkgInstallUtil {
                     if (document != null) {
                         val pomBackupContent = document.text
                         if (mavenCommand != null) {
-                            val groupId =
-                                Regex("<groupId>(.*?)</groupId>").find(mavenCommand)?.groups?.get(1)?.value?.trim()
-                                    ?: ""
-                            val artifactId =
-                                Regex("<artifactId>(.*?)</artifactId>").find(mavenCommand)?.groups?.get(1)?.value?.trim()
-                                    ?: ""
-                            val version =
-                                Regex("<version>(.*?)</version>").find(mavenCommand)?.groups?.get(1)?.value?.trim()
-                                    ?: ""
-
-                            val dependencyPattern =
-                                "<dependency>[\\s\\S]*?<groupId>\\s*$groupId\\s*</groupId>[\\s\\S]*?<artifactId>\\s*$artifactId\\s*</artifactId>[\\s\\S]*?<version>\\s*$version\\s*</version>[\\s\\S]*?</dependency>"
-                            isDependencyExists = pomBackupContent.contains(
-                                dependencyPattern.toRegex(setOf(RegexOption.DOT_MATCHES_ALL, RegexOption.IGNORE_CASE))
-                            )
+                            val commandInfo = parseMavenCommand(mavenCommand)
+                            val version = commandInfo[0]
+                            val dependencyPattern = commandInfo[1]
+                            val matches = Regex(
+                                dependencyPattern,
+                                setOf(RegexOption.DOT_MATCHES_ALL, RegexOption.IGNORE_CASE)
+                            ).findAll(pomBackupContent)
+                            for (match in matches) {
+                                val foundVersion = match.groups[1]?.value?.trim()
+                                if (foundVersion != null) {
+                                    if (foundVersion == version) {
+                                        isDependencyExists = true
+                                        break
+                                    } else {
+                                        needUpdate = true
+                                    }
+                                }
+                            }
                         }
                     }
                 }
             }
-            val resList = mutableListOf<Boolean>()
-            resList.add(isDependencyExists)
-            resList.add(isPomExists)
-            return resList
+            return mutableListOf(isDependencyExists, isPomExists, needUpdate)
+        }
+
+        fun parseMavenCommand(mavenCommand: String): MutableList<String> {
+            val groupId =
+                Regex("<groupId>(.*?)</groupId>").find(mavenCommand)?.groups?.get(1)?.value?.trim()
+                    ?: ""
+            val artifactId =
+                Regex("<artifactId>(.*?)</artifactId>").find(mavenCommand)?.groups?.get(1)?.value?.trim()
+                    ?: ""
+            val version =
+                Regex("<version>(.*?)</version>").find(mavenCommand)?.groups?.get(1)?.value?.trim()
+                    ?: ""
+
+            val dependencyPattern =
+                "<dependency>\\s*?<groupId>\\s*$groupId\\s*</groupId>\\s*?<artifactId>\\s*$artifactId\\s*</artifactId>\\s*?<version>(.*?)</version>\\s*?</dependency>"
+            return mutableListOf(version, dependencyPattern, artifactId)
         }
     }
 }
